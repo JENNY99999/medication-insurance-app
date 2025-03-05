8ff43c8e6f9491cf5aed1a2c1465a1e4ac840c19
JENNY99999 <100115580+JENNY99999@users.noreply.github.com>
2025-03-05 11:14:24 -0800

    Update index.tsx

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,5 @@
-// app/index.tsx
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  return <Redirect href="/home" />; // 重定向到 /home
+  return <Redirect href="/home" />; 
 }
